101dcf957f7b178c9cfa78f4c3501f37844ebe37
CauaCesar02 <cesarcaua02@gmail.com>
2025-05-21 22:46:50 +0000

    primeiro commite

---
+++ saindo_primitivo/saindo_primitivo/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for saindo_primitivo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saindo_primitivo.settings")
+
+application = get_asgi_application()
---
+++ saindo_primitivo/saindo_primitivo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for saindo_primitivo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saindo_primitivo.settings")
+
+application = get_wsgi_application()
