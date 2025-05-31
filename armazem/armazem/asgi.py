61e4801db8d5fe08f21669200f7e3ff9be9960d5
CauaCesar02 <cesarcaua02@gmail.com>
2025-05-31 22:20:41 +0000

    admin criado

--- armazem/armazem/asgi.py
+++ armazem/armazem/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for saindo_primitivo project.
+ASGI config for armazem project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saindo_primitivo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "armazem.settings")
 
 application = get_asgi_application()
--- armazem/armazem/wsgi.py
+++ armazem/armazem/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for saindo_primitivo project.
+WSGI config for armazem project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saindo_primitivo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "armazem.settings")
 
 application = get_wsgi_application()
